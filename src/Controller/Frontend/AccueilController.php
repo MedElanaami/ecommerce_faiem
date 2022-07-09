4240dad0c8e25626ed5e2cf49d57a09ec0b6c753
mohamedelanaami <elanaamimohamed@gmail.com>
2022-07-09 11:23:16 +0100

    integration de template FRONTEND

---
+++ src/Controller/Frontend/AccueilController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Frontend;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController
+{
+    #[Route('/', name: 'app_accueil')]
+    public function index(): Response
+    {
+        return $this-> render('frontend/index.html.twig');
+    }
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig', [
+
+            'page'=>'contact'
+
+        ]);
+    }
+    #[Route('/service', name: 'service')]
+    public function service(): Response
+    {
+
+
+        return $this->render('service.html.twig', [
+
+            'page'=>"service"
+
+        ]);
+    }
+    #[Route('/admin', name: 'admin_accueil')]
+    public function adminAccueil(): Response
+    {
+        return $this->render('accueil/index.html.twig', [
+
+
+        ]);
+    }
+}
