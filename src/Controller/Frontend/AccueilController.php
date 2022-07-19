8239ac18c36a66c39d34034f9ba80467f8371caf
mohamedelanaami <elanaamimohamed@gmail.com>
2022-07-19 12:02:42 +0100

    Afficher un message flash NewsLetter

--- src/Controller/Frontend/AccueilController.php
+++ src/Controller/Frontend/AccueilController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller\Frontend;
 
+use App\Entity\Newsletter;
+use App\Form\NewsletterType;
+use App\Repository\NewsletterRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,17 +15,19 @@ class AccueilController extends AbstractController
     #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
-        return $this-> render('frontend/index.html.twig');
+        return $this->render('frontend/index.html.twig');
     }
+
     #[Route('/contact', name: 'contact')]
     public function contact(): Response
     {
         return $this->render('contact.html.twig', [
 
-            'page'=>'contact'
+            'page' => 'contact'
 
         ]);
     }
+
     #[Route('/service', name: 'service')]
     public function service(): Response
     {
@@ -29,10 +35,11 @@ class AccueilController extends AbstractController
 
         return $this->render('service.html.twig', [
 
-            'page'=>"service"
+            'page' => "service"
 
         ]);
     }
+
     #[Route('/admin', name: 'admin_accueil')]
     public function adminAccueil(): Response
     {
@@ -40,5 +47,22 @@ class AccueilController extends AbstractController
 
 
         ]);
+    }
+
+    #[Route('/newsletter', name: 'newsletter')]
+    public function newsletter(Request $request, NewsletterRepository $newsletterRepository): Response
+    {
+        $newsletter = new Newsletter();
+        $form = $this->createForm(NewsletterType::class, $newsletter);
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted()) {
+            $newsletterRepository->add($newsletter, true);
+            $this->addFlash('success', 'Merci votre inscription a bien été prise en compte!!');
+           return $this->redirect($request->headers->get('referer'));
+
+        }
+        return $this->redirectToRoute('app_accueil');
     }
 }
