81944b972f01a7090bff7a3982f63af28e031ae2
mohamedelanaami <elanaamimohamed@gmail.com>
2022-07-30 14:27:58 +0100

    #11 Ajouter entité livraison

--- src/Form/LivraisonType.php
+++ src/Form/LivraisonType.php
@@ -2,28 +2,25 @@
 
 namespace App\Form;
 
-use App\Entity\Ville;
-use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use App\Entity\Livraison;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class VilleType extends AbstractType
+class LivraisonType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', TextType::class)
-            ->add('livraison');
+            ->add('prix')
 
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Ville::class,
+            'data_class' => Livraison::class,
         ]);
     }
 }
